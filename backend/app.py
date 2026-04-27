9f801552619fc6669a09072ea2fb605658f34a31
aditya-1408 <1408asingh@gmail.com>
2026-04-28 05:02:10 +0530

    app.run() was updated here to allow the app to be accessible from outside the local machine, which is necessary for deployment on platforms like Heroku. The host was changed from "

--- backend/app.py
+++ backend/app.py
@@ -6597,4 +6597,4 @@ def file_too_large(_error):
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
